7aa431399de56f70bc30ce13886ddef9bb1ae5c5
haoxiaotian <haoxiaotian@gmail.com>
2015-07-12 03:20:22 +0800

    GitHub Test

---
+++ GitHubTest.cpp
@@ -0,0 +1,14 @@
+// GitHubTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello GitHub" << endl;
+
+	return 0;
+}
+
